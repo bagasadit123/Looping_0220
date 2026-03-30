eb0f5fccc701ada6f5402bdb93bc0e30c06fc307
bagasadit123 <bagasadit430@gmail.com>
2026-03-30 18:47:23 +0700

    hasil hitungan

--- looping.cpp
+++ looping.cpp
@@ -55,5 +55,10 @@ int main() {
         status = "Lulus";
     else
         status = "Tidak lulus";
+
+    cout << "Status kelulusan = " << status
+         << ", dengan nilai rata-rata = " << rerata << endl;
+
+    return 0;
 }
     
