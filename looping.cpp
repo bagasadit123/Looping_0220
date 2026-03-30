062f9c3dc251d36cc5c3148773789bf207b34893
bagasadit123 <bagasadit430@gmail.com>
2026-03-30 18:46:18 +0700

    menambahkan keterangan status

--- looping.cpp
+++ looping.cpp
@@ -50,5 +50,10 @@ int main() {
     cin >> nilB;
  
     rerata = (nilB + nilM) / 2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
 }
     
