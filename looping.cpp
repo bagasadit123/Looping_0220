d103ac2e3d8c843111173b67e43441cc5d502b00
bagasadit123 <bagasadit430@gmail.com>
2026-03-30 19:28:23 +0700

    menghitung

--- looping.cpp
+++ looping.cpp
@@ -93,5 +93,13 @@ using namespace std;
 int main() {
     float nilB, nilM, rerata;
     string status;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilB;
+
+    rerata = (nilB + nilM) / 2;
+
 }
     
